8b0950fbc502c2197f0323e189f70f6e7d7558e1
Jet (Ta-Wei Chien) <67193495+jet-c-21@users.noreply.github.com>
2025-03-05 01:44:30 +0800

    Add photo_count attribute to PhotoAlbum and update __repr__ method

--- iphone_photos_manager/photo_entities/photo_album.py
+++ iphone_photos_manager/photo_entities/photo_album.py
@@ -9,15 +9,22 @@ from typing import Optional
 
 
 class PhotoAlbum:
-    def __init__(self, title: str, uuid: str, pk_in_z_generic_album: Optional[int] = None,
-        created_datetime: Optional[datetime.datetime] = None,):
+    def __init__(
+        self,
+        title: str,
+        uuid: str,
+        pk_in_z_generic_album: Optional[int] = None,
+        created_datetime: Optional[datetime.datetime] = None,
+        photo_count: Optional[int] = None,
+    ):
         self.title = title
         self.uuid = uuid
         self.pk_in_z_generic_album = pk_in_z_generic_album
         self.created_datetime = created_datetime
+        self.photo_count = photo_count
 
     def __repr__(self):
-        s = f"<{self.__class__.__name__}:{self.title}>"
+        s = f"<{self.__class__.__name__}:{self.title} [{self.photo_count}]>"
         return s
 
     def view_info(self):
